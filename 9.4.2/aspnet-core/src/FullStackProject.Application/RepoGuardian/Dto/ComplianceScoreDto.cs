ac692f6117ba7e513f2fa8d0b69f123e82b451d5
vuyani02 <146190337+vuyani02@users.noreply.github.com>
2026-03-27 15:32:07 +0200

    feat: add ComplianceScoreDto DTO

---
+++ 9.4.2/aspnet-core/src/FullStackProject.Application/RepoGuardian/Dto/ComplianceScoreDto.cs
@@ -0,0 +1,8 @@
+namespace FullStackProject.RepoGuardian.Dto
+{
+    public class ComplianceScoreDto
+    {
+        public string Category { get; set; }
+        public int Score { get; set; }
+    }
+}
